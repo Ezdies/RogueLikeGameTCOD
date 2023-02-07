73d633a2b6ee710e987ac109f2c1beddc72c7e97
Ezdies <bvbclubbvb@gmail.com>
2023-02-07 02:02:58 +0100

    Added fog of war and lights

--- engine.py
+++ engine.py
@@ -6,6 +6,7 @@ from tcod.console import Console
 from entity import Entity
 from input_handlers import EventHandler
 from game_map import GameMap
+from tcod.map import compute_fov
 
 
 class Engine:
@@ -14,6 +15,8 @@ class Engine:
         self.event_handler = event_handler
         self.game_map = game_map
         self.player = player
+        self.update_fov()
+
 
     def handle_events(self, events: Iterable[Any]) -> None:
         for event in events:
@@ -23,12 +26,23 @@ class Engine:
                 continue
 
             action.perform(self, self.player)
+            self.update_fov()  # Update the FOV before the next action.
+
+    def update_fov(self) -> None:
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles["transparent"],
+            (self.player.x, self.player.y),
+            radius=8
+        )
+        self.game_map.explored |= self.game_map.visible
+
 
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
 
         for entity in self.entities:
-            console.print(x=entity.x, y=entity.y, string=entity.char, fg=entity.color)
+            if self.game_map.visible[entity.x, entity.y]:
+                console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
         context.present(console)
 
