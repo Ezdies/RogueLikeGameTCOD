896b933e2d507b15a16645f030683be9a9b43a78
Ezdies <bvbclubbvb@gmail.com>
2023-02-07 03:11:14 +0100

    enemies are doing something

--- game_map.py
+++ game_map.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import numpy as np
 from tcod.console import Console
-from typing import Iterable, TYPE_CHECKING
+from typing import Iterable, Optional, TYPE_CHECKING
 
 import tile_types
 
@@ -17,6 +17,14 @@ class GameMap:
 
         self.visible = np.full((width, height), fill_value=False, order="F")
         self.explored = np.full((width, height), fill_value=False, order="F")
+
+
+    def get_blocking_entity_at_location(self, location_x: int, location_y: int) -> Optional[Entity]:
+        for entity in self.entities:
+            if entity.blocks_movement and entity.x == location_x and entity.y == location_y:
+                return entity
+
+        return None
 
     def in_bounds(self, x: int, y: int) -> bool:
         return 0 <= x < self.width and 0 <= y < self.height
